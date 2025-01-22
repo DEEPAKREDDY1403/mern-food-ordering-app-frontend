0ace95b4d82a979365a801ff89ce775ded3354b2
DEEPAKREDDY1403 <168168119+DEEPAKREDDY1403@users.noreply.github.com>
2025-01-22 23:16:17 +0530

    Update MyUserApi.tsx

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
-import { User } from "../../src/types";
+import { User } from "@/types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
