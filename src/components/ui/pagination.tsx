a0bd8939c389c6b5dec00237d4190009a1770a7f
DEEPAKEDDY1403 <deepakreddy217@gmail.com>
2025-01-29 20:33:46 +0530

    changes in button.tsx and paginatio.tsx

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
 
-import { cn } from "@/lib/utils"
-import { ButtonProps, buttonVariants } from "@/components/ui/button"
+import { cn } from "../../lib/utils";
+import { ButtonProps, buttonVariants } from "../../components/ui/button"
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
@@ -59,20 +59,18 @@ const PaginationLink = ({
 )
 PaginationLink.displayName = "PaginationLink"
 
-const PaginationPrevious = ({
-  className,
-  ...props
-}: React.ComponentProps<typeof PaginationLink>) => (
+const PaginationPrevious = ({ className, size, ...props }: PaginationLinkProps) => (
   <PaginationLink
     aria-label="Go to previous page"
-    size="default"
+    size={size ?? "default"} 
     className={cn("gap-1 pl-2.5", className)}
     {...props}
   >
     <ChevronLeft className="h-4 w-4" />
     <span>Previous</span>
   </PaginationLink>
-)
+);
+
 PaginationPrevious.displayName = "PaginationPrevious"
 
 const PaginationNext = ({
